ec8df58fc65168c6bc16a606bda95e8c8a9a73bb
Yannic <yannic.kindermann@gmail.com>
2023-01-29 01:38:53 +0100

    new test.py file

---
+++ scripts/test.py
@@ -0,0 +1,27 @@
+def pasrse_args():
+    parser = argparse.ArgumentParser(
+        description='mmseg test (and eval) a model')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument(
+        '--eval',
+        type=str,
+        nargs='+',
+        help='evaluation metrics, which depends on the dataset, e.g., "mIoU"'
+        ' for generic datasets, and "cityscapes" for Cityscapes')
+    parser.add_argument('--show', action='store_true', help='show results')
+    parser.add_argument(
+        '--show-dir', help='directory where painted images will be saved')
+    parser.add_argument(
+        '--opacity',
+        type=float,
+        default=0.5,
+        help='Opacity of painted segmentation map. In (0, 1] range.')
+
+    return args
+
+
+
+
+if __name__ == '__main__':
+    args = pasrse_args()
